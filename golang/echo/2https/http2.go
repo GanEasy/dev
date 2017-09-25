fb821ab9697c1df300879d0df051a5678c343394
Administrator <245561237@qq.com>
2017-09-25 17:50:48 +0800

    http 2 https

---
+++ golang/echo/2https/http2.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Pre(middleware.HTTPSRedirect())
+	e.Logger.Fatal(e.Start(":80"))
+}
