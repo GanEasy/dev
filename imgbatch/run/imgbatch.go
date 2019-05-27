4ecbbe246630b95edc72839d0fb56add9a5ab412
yizenghui <245561237@qq.com>
2019-05-27 09:24:19 +0800

    批量搬图片

--- imgbatch/run/imgbatch.go
+++ imgbatch/run/imgbatch.go
@@ -25,23 +25,37 @@ func main() {
 	dir := imgbatch.GetCurrentDirectory()
 	i := 0
 	dirName = imgbatch.GetThisDirName(dir + `/`)
-	tt, _ := imgbatch.GetDirAllImgFile(dir)
+	tt, _ := imgbatch.GetDirAllImgFile(dir) // 获取当前目录所有文件
 	for _, ed := range echoDirs {
 		i = 1
 		for _, file := range tt {
 			i++
 			from = fmt.Sprintf("%s/%s", dir, file.Name())
-			to = fmt.Sprintf("%s/%s/%s%s-1-%v%s", dir, ed, dirName, ed, i, path.Ext(file.Name()))
+			to = fmt.Sprintf("%s/%s/%s%s-%v%s", dir, ed, dirName, ed, i, path.Ext(file.Name()))
 			imgbatch.Copy(from, to)
 		}
-
 	}
 
-	i = 1
-	for _, file := range tt {
+	i = 0
+
+	ttc, _ := imgbatch.GetDirAllImgFile(dir + `/1`) // 获取文件夹1(首图文件夹，所有图片)
+	i = 0
+	for k, file := range ttc {
+		// fmt.Println("echoDirs", echoDirs[k])
+		i++
+		from = fmt.Sprintf("%s/%s", dir+`/1`, file.Name())
+		to = fmt.Sprintf("%s/%s/%s-%v%s", dir, echoDirs[k], dirName, 1, path.Ext(file.Name()))
+		imgbatch.Copy(from, to)
+
+		from = fmt.Sprintf("%s/%s", dir+`/1`, file.Name())
+		to = fmt.Sprintf("%s/%s/%s-%v%s", dir, `全部图片`, dirName, i, path.Ext(file.Name()))
+		imgbatch.Copy(from, to)
+	}
+
+	for _, file := range tt { // 将所有图片放入
 		i++
 		from = fmt.Sprintf("%s/%s", dir, file.Name())
-		to = fmt.Sprintf("%s/%s/%s-1-%v%s", dir, `全部图片`, dirName, i, path.Ext(file.Name()))
+		to = fmt.Sprintf("%s/%s/%s-%v%s", dir, `全部图片`, dirName, i, path.Ext(file.Name()))
 		imgbatch.Copy(from, to)
 	}
 
